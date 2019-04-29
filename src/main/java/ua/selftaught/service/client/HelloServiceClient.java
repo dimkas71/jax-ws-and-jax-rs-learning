1f0170663507512d3f6c55ba57352c44a3cf483d
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-04-29 14:12:50 +0300

    Change the way of HelloService creation
    Hello service is creating through qName and url for
    webservice instead of usinng HelloServiceImplService.

--- src/main/java/ua/selftaught/service/client/HelloServiceClient.java
+++ src/main/java/ua/selftaught/service/client/HelloServiceClient.java
@@ -1,15 +1,30 @@
 package ua.selftaught.service.client;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import ua.selftaught.service.HelloService;
 public class HelloServiceClient {
 
-	public static void main(String[] args) {
-		HelloService client = new HelloServiceImplService().getHelloServiceImplPort();
+	public static void main(String[] args) throws MalformedURLException {
+		
+		final String NAMESPACE = "http://service.selftaught.ua/";
+		URL url = new URL("http://localhost:1986/wss/hello?wsdl");
+		
+		QName qName = new QName(NAMESPACE, "HelloServiceImplService");
+		
+		Service service = Service.create(url, qName);
+		
+		HelloService client = service.getPort(HelloService.class);
+		
 		
 		try {
 			List<String> names = Files.readAllLines(
